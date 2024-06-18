72ff0debb5f17cadf5d541d533b39ed100ade403
Tod Detre <tod.detre@maine.edu>
2024-06-18 08:15:37 -0400

    use quote_plus

--- plugins/modules/splunk_api.py
+++ plugins/modules/splunk_api.py
@@ -49,7 +49,8 @@ def api_call_tcp(cert_prefix, method, endpoint, username, password, svc_port, pa
     return response, excep_str
 
 def api_call_uds(method, endpoint, username, password, svc_port, uds_socket_path, payload=None, headers=None, verify=False, status_code=None, timeout=None, body_format=None):
-    url = "http+unix://{}{}".format(urllib.parse.quote_plus(uds_socket_path),endpoint)
+    #url = "http+unix://{}{}".format(urllib.parse.quote_plus(uds_socket_path),endpoint)
+    url = "http+unix://{}{}".format(UDS_SOCKET_PATH_URL,endpoint)
     if headers is None:
         headers = {}
     headers['Content-Type'] = 'application/json'
